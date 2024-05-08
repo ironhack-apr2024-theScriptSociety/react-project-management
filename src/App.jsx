eb11dda10d236e0256670234b62aa2c4275a5f28
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-05-08 12:14:52 +0200

    implement functionality for project details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar'
 import HomePage from "./pages/HomePage"
 import ProjectListPage from "./pages/ProjectListPage"
 import CreateProjectPage from "./pages/CreateProjectPage"
+import ProjectDetailsPage from "./pages/ProjectDetailsPage"
+
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectListPage />} />
         <Route path="/projects/create" element={<CreateProjectPage />} />
+        <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
         <Route path="*" element={<h2>Page not found</h2>} />
       </Routes>
 
---
+++ src/pages/ProjectDetailsPage.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { API_URL } from "../utils/constants"
+
+
+function ProjectDetailsPage() {
+
+    const [project, setProject] = useState(null);
+
+    const { projectId } = useParams();
+
+    const getProject = () => {
+        axios
+            .get(`${API_URL}/projects/${projectId}?_embed=tasks`)
+            .then((response) => {
+                setProject(response.data);
+            })
+            .catch(e => console.log("error getting project details from API", e))
+    };
+
+
+    useEffect(() => {
+        getProject();
+    }, []);
+
+
+    if (project === null) {
+        return <div className="loader"></div>;
+    }
+
+    return (
+        <div className="ProjectDetailsPage">
+
+            {project && (
+                <>
+                    <h1>{project.title}</h1>
+                    <p>{project.description}</p>
+                </>
+            )}
+
+            {project &&
+                project.tasks.map((task) => (
+                    <li className="TaskCard card" key={task.id}>
+                        <h3>{task.title}</h3>
+                        <h4>Description:</h4>
+                        <p>{task.description}</p>
+                    </li>
+                ))}
+
+            <Link to="/projects">
+                <button>Back to projects</button>
+            </Link>
+
+        </div>
+    );
+}
+
+export default ProjectDetailsPage;
